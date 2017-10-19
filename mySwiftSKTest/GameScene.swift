2f757371840090b2bfcb4fd70629c5727a5bc520
Robert Zimmelman <zimmelman@gmail.com>
2017-10-19 07:56:39 -0400

    working

--- mySwiftSKTest/GameScene.swift
+++ mySwiftSKTest/GameScene.swift
@@ -9,69 +9,166 @@
 import SpriteKit
 import GameplayKit
 
+
 class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    private var pictureNode : SKSpriteNode?
+    private let myRandomDist = GKRandomDistribution(lowestValue: 0, highestValue: 100)
+    private let myParticleSystem = SKEmitterNode(fileNamed: "MyParticle.sks")
+    private let myStrokeImage = UIImage(named: "donald-trump")
+//    private var myLevelSprite = SKSpriteNode()
+
+    
+//    private let myStrokeTexture = SKTexture(imageNamed: "donald-trump")
+    
     
     override func didMove(to view: SKView) {
-        
+        //        NotificationCenter .default.addObserver(self, selector: #selector(myChangeFadeValue), name: NSNotification.Name(rawValue: "changeFadeValue"), object: Any?.self)
+        let myLongPressGR = UILongPressGestureRecognizer(target: self, action: #selector(GameScene.myLongPressRecognized))
+        myLongPressGR.minimumPressDuration = 2.0
+        self.view?.addGestureRecognizer(myLongPressGR)
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
+        
+        let w = (self.size.width + self.size.height) * 0.02
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
         if let spinnyNode = self.spinnyNode {
+            spinnyNode.name = "trails"
             spinnyNode.lineWidth = 2.5
             
             spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
+            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: Data.gameScene.myFadeDuration),
+                                              SKAction.fadeOut(withDuration: Data.gameScene.myFadeDuration),
                                               SKAction.removeFromParent()]))
         }
     }
     
+//    @objc func myChangeFadeValue() {
+//        print("in myChngeFadeValue")
+//    }
     
+    
+    @objc func myLongPressRecognized() {
+        print("Long Press")
+        print("Fade Duration is \(Data.gameScene.myFadeDuration)")
+        scene?.enumerateChildNodes(withName: "trails", using: { (node, stop) in
+            node.removeFromParent()
+        })
+    }
+
     func touchDown(atPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            //            let myRandomDist = GKRandomDistribution(lowestValue: 0, highestValue: 100)
+            let myR = myRandomDist.nextInt()
+            let myG = myRandomDist.nextInt()
+            let myB = myRandomDist.nextInt()
+            let myRed = CGFloat(myR) / 100.0
+            let myGreen = CGFloat(myG) / 100.0
+            let myBlue = CGFloat(myB) / 100.0
+            let myRandomColor = SKColor(red: myRed, green: myGreen, blue: myBlue, alpha: 1.0)
+            n.setScale(0.25)
             n.position = pos
-            n.strokeColor = SKColor.green
+            n.strokeColor = myRandomColor
+            n.fillColor = myRandomColor
             self.addChild(n)
-        }
+            }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
             n.position = pos
-            n.strokeColor = SKColor.blue
+            //            let myRandomDist = GKRandomDistribution(lowestValue: 0, highestValue: 100)
+            let myR = myRandomDist.nextInt()
+            let myG = myRandomDist.nextInt()
+            let myB = myRandomDist.nextInt()
+            let myRed = CGFloat(myR) / 100.0
+            let myGreen = CGFloat(myG) / 100.0
+            let myBlue = CGFloat(myB) / 100.0
+            let myRandomColor = SKColor(red: myRed, green: myGreen, blue: myBlue, alpha: 1.0)
+            n.strokeColor = myRandomColor
+            n.glowWidth = 5.0
+            n.lineWidth = 4.0
+//            n.strokeTexture = myStrokeTexture
             self.addChild(n)
+            n.run(.fadeOut(withDuration: Data.gameScene.myFadeDuration))
         }
     }
     
+    func myRandomColor() -> SKColor {
+        let myR = myRandomDist.nextInt()
+        let myG = myRandomDist.nextInt()
+        let myB = myRandomDist.nextInt()
+        let myRed = CGFloat(myR) / 100.0
+        let myGreen = CGFloat(myG) / 100.0
+        let myBlue = CGFloat(myB) / 100.0
+        let theRandomColor = SKColor(red: myRed, green: myGreen, blue: myBlue, alpha: 1.0)
+        return theRandomColor
+    }
+    
     func touchUp(atPoint pos : CGPoint) {
+        let myLevelSprite = self.childNode(withName: "LevelMeter")
+        myLevelSprite?.xScale = 0.01
+        let myStrokeTexture = SKTexture(imageNamed: "hillary-clinton.png")
+        let myTextureNode = SKSpriteNode(texture: myStrokeTexture)
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            myTextureNode.position = pos
+            myTextureNode.scale(to: CGSize(width: 50.0, height: 50.0))
+            self.addChild(myTextureNode)
             n.position = pos
-            n.strokeColor = SKColor.red
             self.addChild(n)
+            let mySpinAction = SKAction.rotate(byAngle: 360, duration: 2.0)
+            let myScaleDownAction = SKAction.scale(to: 0.01, duration: 2.0)
+            let mySpinAndScaleDownAction = SKAction.group([mySpinAction,myScaleDownAction])
+            let myFadeOutAction = SKAction.fadeOut(withDuration: 0.5)
+            n.run(SKAction.sequence([mySpinAndScaleDownAction,myFadeOutAction]))
+            myTextureNode.run(SKAction.sequence([SKAction .wait(forDuration: 0.25), SKAction .removeFromParent()]))
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+            label.run(SKAction.rotate(toAngle: 0.0, duration: 0.5))
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        for t in touches {
+            self.touchDown(atPoint: t.location(in: self))
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        let myLevelSprite = self.childNode(withName: "LevelMeter")
+        let myFadeAndRemove = SKAction .sequence([SKAction .scale(to: 0.1, duration: 0.5), SKAction .removeFromParent()])
+        for t in touches {
+            myLevelSprite?.xScale = (t.force)
+            self.touchMoved(toPoint: t.location(in: self))
+            if Data.gameScene.my3DTouchAvailable == true {
+                print("Force = \(t.force)")
+                if t.force > 2.0 {
+                    let myPS = myParticleSystem?.copy() as! SKEmitterNode
+                    myPS.position = t.location(in: self)
+                    myPS.setScale(2.0)
+                    myPS.name = "trails"
+                    self.addChild(myPS)
+                    myPS.run(myFadeAndRemove)
+                }
+                if t.force > 4.0 {
+                    let myPS = myParticleSystem?.copy() as! SKEmitterNode
+                    myPS.position = t.location(in: self)
+                    myPS.setScale(10.0)
+                    myPS.particleColor = myRandomColor()
+                    myPS.name = "trails"
+                    self.addChild(myPS)
+                    myPS.run(myFadeAndRemove)
+                }
+            }
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
--- mySwiftSKTest/GameViewController.swift
+++ mySwiftSKTest/GameViewController.swift
@@ -9,24 +9,55 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+class Data {
+    struct gameScene {
+        static var myFadeDuration:Double = 2.0
+        static var mySpinDuration = 1.0
+        static var my3DTouchAvailable = false
+        static var scene:SKScene = SKScene()
+    }
+}
+
+var gameScene: GameScene!
 
 class GameViewController: UIViewController {
-
+    var myTrailDuration = Data.gameScene.myFadeDuration
+    
+    @IBOutlet weak var myTrailDurationLabel: UILabel!
+    @IBOutlet weak var myTrailDurationStepper: UIStepper!
+    @IBAction func myStepperValueChanged(_ sender: Any) {
+        print("changing stepper value")
+        myTrailDuration = myTrailDurationStepper.value
+        myTrailDurationLabel.text = String(format: "%f", myTrailDuration)
+        Data.gameScene.myFadeDuration = myTrailDuration
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        gameScene = GameScene()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Force Touch Raw Value = \(self.traitCollection.forceTouchCapability.rawValue)")
         
+        
+        if self.traitCollection.forceTouchCapability.rawValue == 2  {
+            Data.gameScene.my3DTouchAvailable = true
+        }
+
+        myTrailDuration = myTrailDurationStepper.value
+        myTrailDurationLabel.text = String(format: "%f", myTrailDuration)
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
                 // Present the scene
                 view.presentScene(scene)
+                Data.gameScene.scene = scene
             }
-            
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
         }
